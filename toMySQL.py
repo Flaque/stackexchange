2fa3c6cae6025668d562a59f4f187a86a6e9e85a
Flaqueeau <econrad@zagmail.gonzaga.edu>
2016-11-28 16:40:05 -0800

    Added insert_answer

--- toMySQL.py
+++ toMySQL.py
@@ -10,8 +10,18 @@ def insert_question(db, sitename, stackexchange_id, score, view_count, body):
     """
     db.query(query % (sitename, stackexchange_id, score, view_count, body))
 
-def insert_answer(db, question_id):
-    pass
+def insert_answer(db, sitename, question_stackexchange_id, stackexchange_id, score, body):
+    query = """
+        INSERT INTO Answers
+            SET question_id=(
+                SELECT id FROM Questions WHERE stackexchange_id='%s'
+                AND site_id= (
+                    SELECT id FROM Sites WHERE sitename='%s'
+                )
+            ),
+            stackexchange_id='%s', score='%s', body='%s'
+    """
+    db.query(query % (question_stackexchange_id, sitename, stackexchange_id, score, body))
 
 def insert_site(db, name):
     query = 'INSERT INTO Sites (sitename) VALUES ("%s");' % (name)
@@ -22,7 +32,8 @@ def connect():
 
 def main():
     db = connect()
-    insert_site(db, 'testtwo')
-    insert_question(db, 'testtwo', 1, 10, 100, 'hey there bro whats up?')
+    #insert_site(db, 'testtwo')
+    #insert_question(db, 'testtwo', 1, 10, 100, 'hey there bro whats up?')
+    #insert_answer(db, 'testtwo', 1, 2, 10, 'not much man')
 
 main()
