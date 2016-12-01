75a5c15646836e4bc90ec606e5d37a0ca9d67bfb
Flaqueeau <econrad@zagmail.gonzaga.edu>
2016-12-01 09:05:03 -0800

    Added a summary method

--- lsa.py
+++ lsa.py
@@ -47,15 +47,52 @@ def parse_documents(corpus_file):
         documents.append(strip_text(line))
     return documents
 
+def split_sentences(text):
+    sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+    stripped = [s.strip() for s in sentences]
+
+    returnable = []
+    for s in stripped:
+        if s:
+            returnable.append(s)
+    return returnable
+
 def main():
-    with open('test_corpus.txt', 'r') as _corpus:
-        documents = parse_documents(_corpus)
+    k = 10
+
+    with open('doc.md', 'r') as _corpus:
+        text = ''.join(_corpus.readlines())
+        sentences = split_sentences(text)
 
         sims = []
-        for i in range(0, len(documents)):
-            for j in range(i, len(documents)):
-                sims.append(sim(documents[i], documents[j]))
+        for i in range(0, len(sentences)-1):
+            for j in range(i, len(sentences)):
+                sim_val = sim(sentences[i], sentences[j])
 
-        print sorted(sims)
+                sims.append(((i, j, sentences[i], sentences[j]), sim_val))
+
+        length = len(sims)
+        sims_sorted = sorted(sims, key=operator.itemgetter(1))
+
+        for i in range(0, k):
+            del sims_sorted[-1]
+
+        new_text = [''] * length
+        for sentence, value in sims_sorted:
+            i, j, sen_1, sen_2 = sentence
+            new_text[i] = sen_1 + '. \n'
+
+
+        print ''.join(new_text)
+
+    # with open('test_corpus.txt', 'r') as _corpus:
+    #     documents = parse_documents(_corpus)
+    #
+    #     sims = []
+    #     for i in range(0, len(documents)):
+    #         for j in range(i, len(documents)):
+    #             sims.append(sim(documents[i], documents[j]))
+    #
+    #     print sorted(sims)
 
 main()
